edecbe45d8e6219286f49763a731e22aacb2e31b
Harsh Tomar <tomarharsh28303@gmail.com>
2025-12-13 11:19:16 +0530

    project structure setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A production-grade MLOps capstone project demonstrating the complete machine learning lifecycle—from data versioning with DVC, experiment tracking with MLflow/DagsHub, containerization with Docker, to deployment on AWS EKS with CI/CD pipelines and observability using Prometheus & Grafana.',
+    author='kernel crush',
+    license='MIT',
+)
