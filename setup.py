e7da7d7a0bc43bc4c05d5766ce8b95b11e962c68
Harsh Tomar <tomarharsh28303@gmail.com>
2025-12-18 13:46:45 +0530

    gitignore file

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(
     version='0.1.0',
     description='A production-grade MLOps capstone project demonstrating the complete machine learning lifecycle—from data versioning with DVC, experiment tracking with MLflow/DagsHub, containerization with Docker, to deployment on AWS EKS with CI/CD pipelines and observability using Prometheus & Grafana.',
     author='kernel crush',
-    license='MIT',
+    license='Apache License 2.0',
 )
